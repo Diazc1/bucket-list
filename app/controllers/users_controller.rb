8925ed1af60eb608e7354c12898b1ad4a9424a13
Cathy Diaz <Cathydiaz14@gmail.com>
2021-03-30 14:30:53 -0700

    fix file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,6 @@
 class UsersController < ApplicationController
 
-    get '/users/signup' do
-        erb :'/users/signup'
-    end
 
-    post '/users/signup' do
-        @user = User.create(username: params[:username], password: params[:password])
-    end
-
-    get '/users/:id' do
-        erb :'/users/show'
-    end
 
 end
 
