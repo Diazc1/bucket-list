383c624b23219d8c124c479aede2e2e889d22030
Cathy Diaz <Cathydiaz14@gmail.com>
2021-03-31 13:06:29 -0700

    create get travel_destinations route and define an instance variable to create a new instance of the travel destinations controller, render to travel destinations/index. Create index file inside travel destinations inside views

--- app/controllers/travel_destinations_controller.rb
+++ app/controllers/travel_destinations_controller.rb
@@ -1,6 +1,28 @@
 class TravelDestinationsController < ApplicationController
 
+    #READ all travel_destinations
+    get '/travel_destinations' do
+        @travel_destinations = Travel_destination.all
+        erb :index
+    end
 
+
+    #READ 1 travel_destination
+
+
+    #CREATE new travel_destination (render form)
+
+
+    #CREATE new travel_destination (save in db)
+
+
+    #UPDATE 1 travel_destination (render form)
+
+
+    #UPDATE 1 travel_destination (save in db)
+
+
+    #DELETE 1 travel_destination
 
 
 end
