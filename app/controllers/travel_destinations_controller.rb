a2b64d89df4e4ae8a98c748e8c2729d208a6a045
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-01 00:50:23 -0700

    create a get route to render to an edit form, create edit.erb file.

--- app/controllers/travel_destinations_controller.rb
+++ app/controllers/travel_destinations_controller.rb
@@ -7,21 +7,40 @@ class TravelDestinationsController < ApplicationController
     end
 
 
-    #READ 1 travel_destination
+    #CREATE new travel_destination (render form)
 
-    get '/travel_destinations/:id' do
-        @travel_destinations = TravelDestination.find_by_id(params[:id])
-        erb :'travel_destinations/show'
+    get '/travel_destinations/new' do
+        erb :'travel_destinations/new'
     end
 
 
-    #CREATE new travel_destination (render form)
+    #READ 1 travel_destination
 
+    get '/travel_destinations/:id' do
+        @travel_destination = TravelDestination.find_by_id(params[:id])
+        erb :'travel_destinations/show'
+    end
 
     #CREATE new travel_destination (save in db)
 
+    post '/travel_destinations' do 
+        travel_destination = TravelDestination.new(location: params["location"], description: params["description"], rating: params["rating"])
+
+        if travel_destination.save
+            redirect "/travel_destinations/#{travel_destination.id}"
+        else
+            redirect "/travel_destinations/new"
+        end
+        
+    end
+
 
     #UPDATE 1 travel_destination (render form)
+
+    get '/travel_destinations/:id/edit' do
+        @travel_destination = TravelDestination.find_by_id(params[:id])
+        erb :'travel_destinations/edit'
+    end
 
 
     #UPDATE 1 travel_destination (save in db)
