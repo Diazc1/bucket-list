32817be39fd421e320b751ec1451680573d59113
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-02 14:56:39 -0700

    create the signup route FORM and POST. POST route will create a new user, and if that user is saved it will take them to the travel destinations route, if the user is not valid then the user will be sent back to the signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,25 @@ class UsersController < ApplicationController
 
     #signup route FORM
     get '/signup' do
+        erb :'/users/signup'
     end
 
     #signup route POST
     post '/signup' do
+        #take data create a new user
+        user = User.new(params["user"])
+
+        #if that user is good to go, log in, redirect /travel_destinations
+        if user.save
+            session["user_id"] = user.id
+            redirect "/travel_destinations"
+
+        #if user not valid, send back to /signup
+        else
+            redirect "/signup"
+        end
     end
 
-    
+
 end
 
