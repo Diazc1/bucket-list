d34bed03d92428c22f11cdfe0cb3d903e15712fd
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-06 23:56:03 -0700

    add a flash error message so that users wont be able to reuse the same log in credentials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,24 @@
 class UsersController < ApplicationController
 
-    ##handles signup as well as editing or deleteing a user account
 
-
-    #signup route FORM
     get '/signup' do
         redirect_if_logged_in
         erb :"/users/signup"
     end
 
-    #signup route POST
+ 
     post '/signup' do
         redirect_if_logged_in
-        #take data create a new user
+        
         user = User.new(params["user"])
 
-        #if that user is good to go, log in, redirect /travel_destinations
+       
         if user.save
             session["user_id"] = user.id
             redirect "/travel_destinations"
 
-        #if user not valid, send back to /signup
         else
+            flash[:error] = "#{user.errors.full_messages.join(", ")}"
             redirect "/signup"
         end
     end
