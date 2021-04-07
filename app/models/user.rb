166963327b4d55a63d9bd25a66622e25814962ef
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-06 23:08:06 -0700

    add validations here to avoid repititious user input

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :travel_destinations
+
+
+    validates :email, uniqueness: true
+    validates :username, uniqueness: true
 end
+
