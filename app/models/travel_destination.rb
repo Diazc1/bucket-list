4f2937920a0c745e76f9a5148fb80c03406980c6
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-06 23:06:37 -0700

    add validations here to avoid bad user input

--- app/models/travel_destination.rb
+++ app/models/travel_destination.rb
@@ -1,3 +1,7 @@
 class TravelDestination < ActiveRecord::Base
     belongs_to :user
+
+    validates :location, presence: true
+    validates :description, presence: true
+    validates :rating, presence: true
 end
